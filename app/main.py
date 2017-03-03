b0f15ad50067114d2a5cfec54a371f9ff66f600f
Jon Pavelich <jonpavelich@users.noreply.github.com>
2017-03-03 02:00:49 -0800

    Avoid random moves that result in certain death

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 import bottle
 import os
 import random
+import sys
 
+directions = ['up', 'down', 'left', 'right']
+
+DEBUG = False
+
+def debug(msg):
+    if DEBUG:
+        print msg
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -29,21 +37,124 @@ def start():
         'name': 'battlesnake-python'
     }
 
-
 @bottle.post('/move')
 def move():
     data = bottle.request.json
+    
+    snakes = data['snakes']
+    me = next(x for x in snakes if x['id'] == data['you'])
+    
+    candidates = safe_moves(data)
 
-    # TODO: Do things with data
-    directions = ['up', 'down', 'left', 'right']
+    debug("Valid moves are "+str(candidates))
+
+    if len(candidates) is 0:
+        print "No valid moves, suiciding"
+        return {
+            'move': inv_dir(direction(me)),
+            'taunt': 'oops.'
+        }
+
+    move = random.choice(candidates)
+
+    print "Was going "+str(direction(me))+", moving "+str(move)
 
     return {
-        'move': random.choice(directions),
-        'taunt': 'battlesnake-python!'
+        'move': move,
+        'taunt': 'I have no idea where I\'m going!'
     }
 
+# Find moves which are safe (i.e. do not kill us immediately)
+def safe_moves(data):
+    width = data['width']
+    height = data['height']
+    snakes = data['snakes']
+    me = next(x for x in snakes if x['id'] == data['you'])
+    others = [x for x in snakes if not x['id'] == data['you']]
+    head = me['coords'][0]
+    
+    moves = ['up', 'down', 'left', 'right']
+
+    debug("Checking up for collisions")
+
+    n_head = list(head)
+    n_head[1] -= 1
+    if n_head[1] < 0:
+        debug("up collides with wall")
+        moves.remove('up')
+    elif safe_moves_collide(n_head, me, others):
+        moves.remove('up')
+
+    n_head = list(head)
+    n_head[1] += 1
+    if n_head[1] >= height:
+        debug("down collides with wall")
+        moves.remove('down')
+    elif safe_moves_collide(n_head, me, others):
+        moves.remove('down')
+
+    n_head = list(head)
+    n_head[0] -= 1
+    if n_head[0] < 0:
+        debug("left collides with wall")
+        moves.remove('left')
+    elif safe_moves_collide(n_head, me, others):
+        moves.remove('left')
+
+    n_head = list(head)
+    n_head[0] += 1
+    if n_head[0] >= width:
+        debug("right collides with wall")
+        moves.remove('right')
+    elif safe_moves_collide(n_head, me, others):
+        moves.remove('right')
+
+    return moves
+    
+def safe_moves_collide(n_head, me, others):
+    body = me['coords'][1:]
+    if n_head in body:
+            debug("move collides with our body")
+            return True
+    for snake in others:
+        if n_head in snake['coords'][0:-1]:
+            debug("move collides with other snake body")
+            return True
+    return False
+
+
+# Determine the direction a snake is travelling
+def direction(snake):
+    coords = snake['coords']
+    head = coords[0]
+    body = coords[1]
+    debug("Head coords "+str(head))
+    debug("Body coords "+str(body))
+
+    if head[0] > body[0]:
+        return 'right'
+    if head[0] < body[0]:
+        return 'left'
+    if head[1] > body[1]:
+        return 'down'
+    if head[1] < body[1]:
+        return 'up'
+
+# Inverse a direction
+def inv_dir(direction):
+    if direction is 'up':
+        return 'down'
+    if direction is 'down':
+        return 'up'
+    if direction is 'left':
+        return 'right'
+    if direction is 'right':
+        return 'left'
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
+    if 'debug' in sys.argv[1:]:
+        print "Enabled debugging messages"
+        DEBUG = True
     bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
